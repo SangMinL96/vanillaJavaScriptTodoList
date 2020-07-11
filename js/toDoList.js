cea7db229098b23fa1aafeb62e6cf91aa7b31038
SangMinL96 <59109788+SangMinL96@users.noreply.github.com>
2020-07-11 22:01:28 +0900

    toDolist
    * askTodo()
    41-form 기본속성 삭제
    42-form input 값을 받아 선언함
    44-askText(input값을 인자로 보냄)
    
    *askText(text)
    HTML추가
    li테크 id값 부여함
    span Text값을 부여받은 인자값으로 만듬
    
    * toDosObj
    text,id값을 배열로 넣고 toDos[] 푸쉬함
    
    *savaToDo()
    localStorage 값을 저장하여 시간이 지나도 저장하게함.
    JSON.stringify사용이유
    (배열의 값이 문자열로 localStorage 저장되어야함. 사용안할시 Object 표시됨)
    
    *toDoDelete()
    ev.target 이용하여 자신이 선택한 button를 알아냄
    찾아낸 button 부모인 li를 삭제
    
    *toDos.filter 사용하여 자신이 클릭한 값을 뺀 나머지를 cleanToDo() 배열로 받음
    
    *parseInt(li.id)
    스프링이 아닌 오브젝트로 받아야함
    
    * toDos = cleanToDo;
    자신이 클릭한 값을 뺀 배열을 다시 toDos 저장
    
    * loadToDo()
    localStorage.getItem(toDoValue)에 값이있어도 계속 실행되게함
    
    *const parseLocalTodo = JSON.parse(localTodo);
    스프링인 값을 오브젝트값으로 다시 선언함
    
    forEach 이용하여 parseLocalTodo에 텍스트값을 인자로 보내어 text생성시킴

---
+++ js/toDoList.js
@@ -0,0 +1,60 @@
+const toDoForm = document.querySelector(".js-toDoform");
+const toDoInput = document.querySelector(".js-toDoform .toDoInput");
+const toDoText = document.querySelector(".toDoList");
+const toDoValue = "toDoValue";
+let toDos = [];
+function toDoDelete(ev) {
+  const btn = ev.target;
+  const li = btn.parentNode;
+  toDoText.removeChild(li);
+  const cleanToDo = toDos.filter(function (todo) {
+    return todo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDo;
+  saveToDo();
+}
+function saveToDo() {
+  localStorage.setItem(toDoValue, JSON.stringify(toDos));
+}
+function askText(text) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  const btn = document.createElement("button");
+  const newId = toDos.length + 1;
+  toDoText.appendChild(li);
+  li.appendChild(span);
+  li.appendChild(btn);
+  li.id = newId;
+  btn.innerText = "❌";
+  btn.addEventListener("click", toDoDelete);
+  span.innerText = text;
+  const toDosObj = {
+    text: text,
+    id: newId,
+  };
+  toDos.push(toDosObj);
+  saveToDo();
+}
+
+function askTodo() {
+  toDoForm.addEventListener("submit", function (ev) {
+    ev.preventDefault();
+    const textValue = toDoInput.value;
+
+    askText(textValue);
+  });
+}
+function loadToDo() {
+  const localTodo = localStorage.getItem(toDoValue);
+  if (localTodo !== null) {
+    const parseLocalTodo = JSON.parse(localTodo);
+    parseLocalTodo.forEach(function (parseLocalTodo) {
+      askText(parseLocalTodo.text);
+    });
+  }
+}
+function init() {
+  loadToDo();
+  askTodo();
+}
+init();
