bf23094db17fce0089aad41eeab52897df626dbe
SangMinL96 <59109788+SangMinL96@users.noreply.github.com>
2020-07-11 21:41:07 +0900

    clock.js
    자동으로 시계동작코드

---
+++ js/clock.js
@@ -0,0 +1,18 @@
+const clock = document.getElementById("js-clock");
+const clockText = document.getElementById("clock");
+
+function getClock() {
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  clockText.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  } `;
+}
+
+function init() {
+  getClock();
+  setInterval(getClock, 1000);
+}
+init();
